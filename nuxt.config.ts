8045fa64fc4222805511da0f173b97c447e6d51f
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-05 16:27:31 +0300

    chore: configure redirects and install necessary modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2024-11-01",
+    devtools: { enabled: true },
+    modules: [
+      "@nuxt/fonts",
+      "@nuxtjs/supabase",
+      "@nuxt/eslint",
+      "@nuxtjs/turnstile",
+      "@nuxt/ui",
+    ],
+    future: {
+        compatibilityVersion: 4,
+    },
+    supabase: {
+        redirect: true,
+        redirectOptions: {
+            login: "/auth",
+            callback: "/confirm",
+            include: ["/auth"],
+        },
+    },
+});
