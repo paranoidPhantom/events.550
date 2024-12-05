b4ac470d87d7b336f10b1271b8af8c960d6524f7
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-05 22:02:13 +0300

    chore: dependencies and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,12 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     modules: [
-      "@nuxt/fonts",
-      "@nuxtjs/supabase",
-      "@nuxt/eslint",
-      "@nuxtjs/turnstile",
-      "@nuxt/ui",
+        "@nuxt/fonts",
+        "@nuxtjs/supabase",
+        "@nuxt/eslint",
+        "@nuxtjs/turnstile",
+        "@nuxt/ui",
+        "nuxt-aos",
     ],
     future: {
         compatibilityVersion: 4,
