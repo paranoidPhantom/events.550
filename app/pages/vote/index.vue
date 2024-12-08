ddfa985da1a0f38b67cd135939ca0fe005e40ce0
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-08 14:55:57 +0300

    feat: finalise voting flow, add success page

---
+++ app/pages/vote/index.vue
@@ -0,0 +1,334 @@
+<script lang="ts" setup>
+import type { DBRow } from "~~/supabase/utils";
+import type { VoteState } from "~~/types/vote";
+
+definePageMeta({
+    name: "Проголосовать",
+});
+
+const state = reactive<VoteState>({
+    first_name: "",
+    last_name: "",
+    middle_name: "",
+    turnstile: undefined,
+    selection: {},
+});
+
+const identities = useIdentities();
+
+const filteredIdentities = computed(() => {
+    if (!state.first_name && !state.last_name && !state.middle_name)
+        return identities.value;
+    return identities.value?.filter((identity) => {
+        return (
+            (!state.first_name ||
+                identity.first_name.includes(state.first_name)) &&
+            (!state.last_name ||
+                identity.last_name.includes(state.last_name)) &&
+            (!state.middle_name ||
+                identity.middle_name?.includes(state.middle_name))
+        );
+    });
+});
+
+const validIdentity = ref(false);
+const foundMatch = ref<DBRow<"identities">>();
+
+watch(filteredIdentities, (updatedFilteredIdentities) => {
+    if (updatedFilteredIdentities?.length === 1) {
+        const lastEntry = updatedFilteredIdentities[0];
+        if (lastEntry) {
+            foundMatch.value = lastEntry;
+        }
+    }
+});
+
+const nameEnumerators = computed(() => {
+    const first: Set<string> = new Set();
+    const last: Set<string> = new Set();
+    const middle: Set<string> = new Set();
+    filteredIdentities.value?.forEach((identity) => {
+        first.add(identity.first_name);
+        last.add(identity.last_name);
+        if (identity.middle_name) middle.add(identity.middle_name);
+    });
+    return {
+        first: Array.from(first),
+        last: Array.from(last),
+        middle: Array.from(middle),
+    };
+});
+
+const resetState = () => {
+    state.first_name = "";
+    state.last_name = "";
+    state.middle_name = "";
+    foundMatch.value = undefined;
+};
+
+const castOptions = useCastOptions();
+
+const makeSelection = (event: MouseEvent, id: number) => {
+    event.stopPropagation();
+    state.selection[id] = !state.selection[id];
+};
+
+const validSelection = computed(() => {
+    return Object.values(state.selection).filter(Boolean).length === 3;
+});
+
+const alreadyVoted = useCookie<boolean>("voted");
+
+const toast = useToast();
+
+const turnstile = ref();
+
+const submitVote = async () => {
+    try {
+        await $fetch("/api/vote", {
+            method: "POST",
+            body: { ...state, id: foundMatch.value?.id },
+        });
+        alreadyVoted.value = true;
+        navigateTo("/vote/success");
+    } catch (error) {
+        if (turnstile.value) turnstile.value.reset();
+        const { statusCode } = error as { statusCode: number };
+        switch (statusCode) {
+            case 409:
+                toast.add({
+                    icon: "mdi-alert-circle",
+                    title: "Вы уже проголосовали!",
+                    color: "rose",
+                    description:
+                        "Если вы думаете что это ошибка, подойдите к столику поддержки",
+                });
+                break;
+            case 422:
+                toast.add({
+                    icon: "mdi-alert-circle",
+                    title: "Инвалидные данные!",
+                    color: "rose",
+                });
+                break;
+            case 403:
+                toast.add({
+                    icon: "mdi-alert-circle",
+                    title: "Невалидно выполниена капча!",
+                    color: "rose",
+                });
+                break;
+        }
+    }
+};
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 pt-[10%] relative">
+        <TransitionGroup name="slide-in">
+            <div
+                class="space-y-4 transition-all duration-500 px-2"
+                :style="{
+                    height: validIdentity ? '0' : 'auto',
+                    overflow: validIdentity ? 'hidden' : undefined,
+                }"
+            >
+                <h2 class="text-2xl font-semibold">Введите свои данные</h2>
+                <hr class="opacity-10" />
+                <UFormGroup label="Фамилия">
+                    <UInputMenu
+                        v-model="state.last_name"
+                        placeholder="Иванов"
+                        :options="nameEnumerators.last"
+                    />
+                </UFormGroup>
+                <UFormGroup label="Имя">
+                    <UInputMenu
+                        v-model="state.first_name"
+                        placeholder="Иван"
+                        :options="nameEnumerators.first"
+                    />
+                </UFormGroup>
+                <UFormGroup label="Отчество">
+                    <UInputMenu
+                        v-model="state.middle_name"
+                        placeholder="Иванович"
+                        :options="nameEnumerators.middle"
+                    />
+                </UFormGroup>
+            </div>
+            <div v-if="foundMatch" class="flex flex-col gap-4">
+                <h2
+                    class="text-2xl font-semibold"
+                    data-aos="fade-down"
+                    data-aos-delay="200"
+                >
+                    <Transition name="slide-symmetrical" mode="out-in">
+                        <span v-if="!validIdentity">
+                            О {{ foundMatch.first_name }}, это вы?
+                        </span>
+                        <span v-else> Выберете 3 лучших выступления </span>
+                    </Transition>
+                </h2>
+                <UCard>
+                    <div>
+                        <div class="flex gap-4 justify-between mb-6">
+                            <UAvatar
+                                data-aos="fade-left"
+                                data-aos-delay="400"
+                                size="xl"
+                                :src="`https://robohash.org/${foundMatch.id}.png`"
+                            />
+                            <div
+                                class="flex gap-4 items-center"
+                                data-aos="fade-right"
+                                data-aos-delay="600"
+                            >
+                                <h4 class="text-xl">
+                                    {{ foundMatch.last_name }}
+                                    {{ foundMatch.first_name }}
+                                    {{ foundMatch.middle_name }}
+                                </h4>
+                                <UBadge
+                                    class="w-fit"
+                                    variant="subtle"
+                                    size="md"
+                                >
+                                    {{ foundMatch.grade }}
+                                </UBadge>
+                            </div>
+                        </div>
+                        <hr class="opacity-10 mb-6" />
+                        <Transition name="slide-symmetrical" mode="out-in">
+                            <div
+                                v-if="!validIdentity"
+                                class="flex gap-2 items-center justify-between overflow-hidden"
+                            >
+                                <UButton
+                                    label="Да!"
+                                    :disabled="validIdentity"
+                                    @click="validIdentity = true"
+                                />
+                                <UButton
+                                    color="rose"
+                                    label="Это ошибка"
+                                    variant="soft"
+                                    :disabled="validIdentity"
+                                    @click="resetState"
+                                />
+                            </div>
+                            <div
+                                v-else
+                                class="flex gap-2 items-center overflow-hidden"
+                                :style="{
+                                    height: validIdentity ? 'auto' : '0',
+                                }"
+                            >
+                                <UAccordion
+                                    :items="
+                                        castOptions.map((option) => ({
+                                            label: option.name,
+                                            ...option,
+                                        }))
+                                    "
+                                >
+                                    <template #default="{ item }">
+                                        <UButton variant="outline" class="mb-4">
+                                            <UCheckbox
+                                                size="lg"
+                                                :disabled="
+                                                    validSelection &&
+                                                    !state.selection[item.id]
+                                                "
+                                                @click="
+                                                        (event: MouseEvent) =>
+                                                            makeSelection(
+                                                                event,
+                                                                item.id
+                                                            )
+                                                    "
+                                            />
+                                            {{ item.name }}
+                                            <UBadge
+                                                size="sm"
+                                                variant="outline"
+                                                class="ml-auto"
+                                            >
+                                                {{ item.author }}
+                                            </UBadge>
+                                        </UButton>
+                                    </template>
+                                    <template #item="{ item }">
+                                        <UCarousel
+                                            v-slot="{ item: slide }"
+                                            :ui="{
+                                                container: 'rounded-lg',
+                                            }"
+                                            :items="item.image_urls"
+                                        >
+                                            <img
+                                                :src="slide"
+                                                width="300"
+                                                height="400"
+                                                draggable="false"
+                                            />
+                                        </UCarousel>
+                                        <p>{{ item.description }}</p>
+                                    </template>
+                                </UAccordion>
+                            </div>
+                        </Transition>
+                    </div>
+                </UCard>
+            </div>
+            <div v-if="validIdentity">
+                <NuxtTurnstile
+                    v-model="state.turnstile"
+                    :options="{ language: 'ru' }"
+                    ref="turnstile"
+                />
+                <UIcon
+                    name="svg-spinners:ring-resize"
+                    class="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 text-tree-500 -z-10"
+                />
+            </div>
+            <UButton
+                v-if="state.turnstile && validSelection"
+                size="xl"
+                class="w-fit"
+                icon="line-md:file-document-twotone"
+                @click="submitVote"
+            >
+                Проголосовать!
+            </UButton>
+        </TransitionGroup>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.slide-in-leave-to {
+    opacity: 0;
+    scale: 0.5;
+}
+
+.slide-in-enter-from {
+    opacity: 0;
+    translate: 0 100%;
+    scale: 0.5;
+}
+.slide-in-enter-active,
+.slide-in-leave-active {
+    transition: all 0.5s;
+}
+
+.slide-symmetrical-enter-from,
+.slide-symmetrical-leave-to {
+    opacity: 0;
+    scale: 0.95;
+}
+
+.slide-symmetrical-enter-active,
+.slide-symmetrical-leave-active {
+    transition: all 0.5s;
+}
+</style>
---
+++ app/pages/vote/success.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+definePageMeta({
+    name: "Голос учтён",
+});
+
+const graphic = ref();
+
+onMounted(() => {
+    setTimeout(() => {
+        graphic.value?.play();
+    }, 1000);
+});
+</script>
+
+<template>
+    <div class="pt-[10%] flex flex-col items-center gap-4">
+        <video ref="graphic" class="w-48" src="/graphics/christmas.webm" />
+        <h2 class="text-2xl font-semibold">Ваш голос учтён!</h2>
+        <UButton to="/" label="На домашнюю" />
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
