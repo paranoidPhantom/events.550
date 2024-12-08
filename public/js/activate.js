b0a2e62eeead7450ef14c8845396538474c8102d
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-08 14:44:49 +0300

    chore: add snow activation script

---
+++ public/js/activate.js
@@ -0,0 +1,18 @@
+snowfall.start({
+	bg: "#00000000",
+	primary: "#8d90b7",
+	secondary: "#ffffff",
+	density: 100,
+	wave: {
+		frequency: 0.02,
+		amplitude: 1
+	},
+	gravity: {
+		angle: 90,
+		strength: 0.7
+	},
+	wind: {
+		angle: 0,
+		strength: 0
+	}
+})
