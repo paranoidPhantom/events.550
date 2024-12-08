e99f75c45ecb423b65c752686c56d78510cd52b5
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-08 14:56:20 +0300

    feat: add mockup home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const alreadyVoted = useCookie<boolean>("voted");
+</script>
 
 <template>
-    <div></div>
+    <div class="flex flex-col gap-4 pt-[10%]">
+        <UAlert
+            v-if="alreadyVoted"
+            color="tree"
+            variant="subtle"
+            icon="game-icons:vote"
+            title="Вы уже проголосовали!"
+            description="Думаете что это ошибка? Подойдите к столику поддержки!"
+        />
+        <UButton to="/vote" :disabled="alreadyVoted" class="w-fit">
+            Проголосовать
+        </UButton>
+    </div>
 </template>
 
 <style lang="scss" scoped></style>
