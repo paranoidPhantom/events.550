2aa5961e9cc3e857e6706ba0ac543e096b24fccb
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-09 17:41:30 +0300

    feat: add footer with personal branding

---
+++ app/components/branded/footer.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup></script>
+
+<template>
+    <footer class="pb-8">
+        <UCard class="opacity-50 text-sm">
+            <p>
+                По организационным вопросам:
+                <NuxtLink to="mailto:andrei@hudalla.dev"
+                    >andrei@hudalla.dev</NuxtLink
+                >
+            </p>
+            <UDivider class="my-4" />
+            <p>
+                &copy; {{ new Date().getFullYear() }}
+                <NuxtLink to="https://hudalla.dev" external
+                    >Андрей Худалла</NuxtLink
+                >
+            </p>
+        </UCard>
+    </footer>
+</template>
+
+<style lang="scss" scoped></style>
