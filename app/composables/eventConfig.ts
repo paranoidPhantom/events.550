a9c98e63a2c74f25d5f3578f68dff9231a3925bb
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-09 17:42:12 +0300

    feat: add event configuration composable

---
+++ app/composables/eventConfig.ts
@@ -0,0 +1,28 @@
+import type { Database } from "~~/supabase/db";
+import type { DBRow } from "~~/supabase/utils";
+import { useSupabaseClient, useState } from "#imports";
+
+export const useEventConfig = () => {
+    // Get global state
+    const eventConfig = useState<DBRow<"event-config"> | null>(
+        "state_event_config",
+        () => null
+    );
+
+    // Get supabase client
+    const supabase = useSupabaseClient<Database>();
+
+    // If cast options are not loaded, fetch them
+    const fetchEventConfig = async () => {
+        const { data, error } = await supabase
+            .from("event-config")
+            .select()
+            .maybeSingle();
+        if (error) throw error;
+        eventConfig.value = data;
+    };
+
+    if (!eventConfig.value) fetchEventConfig();
+
+    return eventConfig;
+};
