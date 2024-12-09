f9dd1ef90f5d49130499fabb3ec0d40fa34308e7
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-09 18:15:39 +0300

    fix: iphone video plays natively

--- app/pages/vote/success.vue
+++ app/pages/vote/success.vue
@@ -14,7 +14,12 @@ onMounted(() => {
 
 <template>
     <div class="flex flex-col items-center gap-4">
-        <video ref="graphic" class="w-48" src="/graphics/christmas.webm" />
+        <video
+            ref="graphic"
+            playsinline
+            class="w-48"
+            src="/graphics/christmas.webm"
+        />
         <h2 class="text-2xl font-semibold">Ваш голос учтён!</h2>
         <UButton to="/" label="На домашнюю" />
     </div>
