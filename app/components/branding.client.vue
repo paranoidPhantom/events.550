45e10b4fcbd3ec4b621496dc764977aaa411edf1
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-09 17:42:01 +0300

    feat: add website branding "header"

---
+++ app/components/branding.client.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const catchPhrase = "Мероприятия на новом уровне";
+const eventConfig = useEventConfig();
+</script>
+
+<template>
+    <div class="branding flex items-center gap-4">
+        <img
+            v-if="eventConfig?.logo_url"
+            :src="eventConfig?.logo_url"
+            alt="Logo"
+            class="w-24"
+            data-aos="flip-up"
+        />
+        <div class="flex flex-col gap-1">
+            <h1 class="text-4xl" data-aos="flip-down" data-aos-delay="100">
+                events.550
+            </h1>
+            <p data-aos="fade-left" data-aos-delay="200">
+                <span
+                    v-for="(word, index) in catchPhrase.split(' ')"
+                    :key="index"
+                    data-aos="fade"
+                    :data-aos-delay="200 + (index + 1) * 100"
+                    >{{ word }}&nbsp;</span
+                >
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.branding {
+    font-family: Poiret One;
+}
+</style>
