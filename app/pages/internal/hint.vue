18c7fd4dae614d2d73927e64f580dcb49bcbd78c
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-15 20:33:03 +0300

    chore: add empty state mirror pages

---
+++ app/pages/internal/hint.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+definePageMeta({
+    layout: "empty",
+});
+
+const items = [
+    "⬇︎ 6Б уходит со сцены",
+    "... бла-бла-бла ...",
+    "⬆︎ 7Б заходит на сцену",
+    "☝️ Встаём на начальные позиции",
+    "🚀 Танцуем",
+];
+
+const currentIndex = ref(3);
+</script>
+
+<template>
+    <div class="flex flex-col justify-center gap-4 mx-[10%] h-screen" style="">
+        <p
+            v-for="(item, index) in items"
+            :key="index"
+            class="transition-all"
+            :style="{
+                opacity: index === currentIndex ? 1 : 0.5,
+                fontSize: index === currentIndex ? '5rem' : '1rem',
+            }"
+        >
+            {{ item }}
+        </p>
+        <UButton
+            class="w-fit"
+            @click="currentIndex = (currentIndex + 1) % items.length"
+            >Next
+        </UButton>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ app/pages/internal/project.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+definePageMeta({
+    layout: "empty",
+});
+</script>
+
+<template>
+    <div />
+</template>
+
+<style lang="scss" scoped></style>
