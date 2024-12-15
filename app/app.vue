31fa0f284c822177dc6680558e19dc76d6cf7ce4
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-15 20:29:53 +0300

    feat: opt in to layouts

--- app/app.vue
+++ app/app.vue
@@ -64,16 +64,8 @@ const snowDisabled = useCookie<boolean>("snow");
         <UNotifications />
 
         <!-- Actual content -->
-        <div
-            class="px-4 sm:px-8 w-full mx-auto max-w-[800px] pt-[10%] space-y-2"
-        >
-            <Branding />
-            <hr class="opacity-10" />
-            <AdminNavigation v-if="user" />
+        <NuxtLayout>
             <NuxtPage />
-            <BrandedFooter />
-        </div>
+        </NuxtLayout>
     </div>
 </template>
-
-<style lang="scss"></style>
---
+++ app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const user = useSupabaseUser();
+</script>
+
+<template>
+    <div class="px-4 sm:px-8 w-full mx-auto max-w-[800px] pt-[10%] space-y-2">
+        <Branding />
+        <hr class="opacity-10" />
+        <AdminNavigation v-if="user" />
+        <slot />
+        <BrandedFooter />
+    </div>
+</template>
+
+<style lang="scss"></style>
