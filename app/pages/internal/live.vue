c9add99c63a6ffa81bd24bcb9dac07694e854e13
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-21 19:49:56 +0300

    feat: start work on stream window overlay

--- app/pages/internal/live.vue
+++ app/pages/internal/live.vue
@@ -2,10 +2,26 @@
 definePageMeta({
     layout: "empty",
 });
+const livestreamCue = useLatestTimelineCue("livestream");
+
+const overlayText = computed(() => {
+    return livestreamCue.value?.livestream?.overlayText;
+});
 </script>
 
 <template>
-    <div />
+    <div class="h-screen flex items-end p-16">
+        <div
+            class="backdrop-blur-lg rounded-2xl px-4 py-2 bg-black bg-opacity-20 border-4 border-black border-opacity-20 flex items-center gap-4"
+        >
+            <img class="w-16" src="/graphics/logo.webp" alt="Logo" />
+            <h1 class="text-3xl font-bold">{{ overlayText }}</h1>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+    font-family: Montserrat;
+}
+</style>
