c3f5a55f506f10245681633b8ef8b3002f6f877e
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-12-26 22:17:13 +0300

    feat: move audio playing to internal page

--- app/pages/internal/project.vue
+++ app/pages/internal/project.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
 import type { DBRow } from "~~/supabase/utils";
 
-const audio = ref<HTMLAudioElement | null>();
-
 definePageMeta({
     layout: "empty",
 });
@@ -10,41 +8,10 @@ definePageMeta({
 const projection = computed(
     () => useEventConfig().value?.state as DBRow<"state">
 );
-
-const updateAudio = (newProjection: (typeof projection)["value"]) => {
-    const newSrc = newProjection?.audio;
-    if (audio.value && newSrc) {
-        const element = audio.value;
-        if (element.getAttribute("src") === newSrc) return;
-        let count = 0;
-        const changeLevels = (direction: "up" | "down") => {
-            element.volume = direction === "down" ? 1 - count / 10 : count / 10;
-            count++;
-            if (count < 10) setTimeout(() => changeLevels(direction), 100);
-            else if (direction === "down") {
-                count = 0;
-                element.setAttribute("src", newSrc);
-                element.play();
-                changeLevels("up");
-            }
-        };
-        changeLevels("down");
-    }
-};
-
-watch(projection, (newProjection) => updateAudio(newProjection));
-
-const stop = watchEffect(() => {
-    if (projection.value && audio.value) {
-        stop();
-        updateAudio(projection.value);
-    }
-});
 </script>
 
 <template>
     <div class="cursor-none h-screen w-screen bg-black">
-        <audio ref="audio" />
         <div v-if="projection?.stageUpdateContent">
             <img
                 v-if="projection?.stageUpdateType === 'image'"
